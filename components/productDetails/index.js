bbbc0ca566fb606c8ea64620438dac316a8de49d
Paul <plslv1991@gmail.com>
2021-04-18 19:28:29 +0530

    eslint and cart ui component added

--- components/productDetails/index.js
+++ components/productDetails/index.js
@@ -1,9 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import Button from '../common/button/index'
 import FavButton from '../common/favButton/index'
 import StarRatings from '../common/productStarRatings/index'
 import ProductColorOptions from '../common/productColorOption'
 import ProductSizeOptions from '../common/productSizeOption'
 import ProductSocialMediaOptions from '../common/productSocialMediaOptions'
+
 function ProductDetails({ productDetails }) {
 
    if (!productDetails) {
@@ -13,7 +16,7 @@ function ProductDetails({ productDetails }) {
       <section className="text-gray-600 body-font overflow-hidden">
          <div className="container px-5 py-24 mx-auto">
             <div className="lg:w-4/5 mx-auto flex flex-wrap">
-               <img alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src={productDetails.image} />
+               <img alt="product" className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src={productDetails.image} />
                <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
                   <h2 className="text-sm title-font text-gray-500 tracking-widest">{productDetails.category}</h2>
                   <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{productDetails.title}</h1>
@@ -39,6 +42,10 @@ function ProductDetails({ productDetails }) {
          </div>
       </section>
    )
-};
+}
+
+ProductDetails.propTypes = {
+   productDetails: PropTypes.string.isRequired
+ }
 
 export default ProductDetails;
